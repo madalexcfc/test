9b027e64e9d7ba1eb31a0702b2eba0f196351d10
madalex <madal3xcfc5@gmail.com>
2024-09-28 22:20:18 +0300

    b

---
+++ flag.py
@@ -0,0 +1,7 @@
+print(f"\u001b[41m{' ' * 20}\u001b[0m")
+for n in range(2):
+    print(f"\u001b[41m{' ' * 8}\u001b[47m{' ' * 4}\u001b[41m{' ' * 8}\u001b[0m")
+print(f"\u001b[41m{' ' * 4}\u001b[47m{' ' * 12}\u001b[41m{' ' * 4}\u001b[0m")
+for n in range(2):
+    print(f"\u001b[41m{' ' * 8}\u001b[47m{' ' * 4}\u001b[41m{' ' * 8}\u001b[0m")
+print(f"\u001b[41m{' ' * 20}\u001b[0m")
